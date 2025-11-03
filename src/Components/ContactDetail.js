7f5b09cdc98ff14c27f472a2e67680199c4b87a6
Raghu Gurram <raghugurram16@gmail.com>
2025-11-03 12:30:55 +0530

    Updated App.js and ContactDetail.js

--- src/Components/ContactDetail.js
+++ src/Components/ContactDetail.js
@@ -1,37 +1,3 @@
-/*import React from "react";
-import { useParams } from "react-router-dom";
-import { Link } from "react-router-dom";
-import user from "../images/user.png";
-
-
-const ContactDetail = ({ contacts }) => {
-  const { id } = useParams();
-  const contact = contacts.find((c) => c.id === id);
-
-  if (!contact) {
-    return <div className="main">Contact not found.</div>;
-  }
-
-  return (
-    <div className="main">
-      <div className="ui card centered">
-        <div className="image">
-          <img src={user} alt="user" />
-        </div>
-        <div className="content">
-          <div className="header">{contact.name}</div>
-          <div className="description">{contact.email}</div>
-        </div>
-        <div className="center-div">
-            <Link to="/"><button className="ui button  blue center">Back to Contact List</button></Link>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default ContactDetail; */
-
 
 import React from "react";
 import { useParams, Link } from "react-router-dom";
